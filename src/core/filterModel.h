4c44b78bf886a9438b32903d0c14925579eb944c
mt382 <mt382@uni-rostock.de>
2020-11-27 00:53:51 +0100

    updated documentation for filter model

--- src/core/filterModel.h
+++ src/core/filterModel.h
@@ -20,8 +20,20 @@ typedef struct Filter
 
 } Filter;
 
+/**
+ * Creates an empty filter and add it to the tail of filterlist.
+ */
 Filter *filter_Create();
 
+/**
+ * Adds a subfilter(aka keyconstrain) to an existing filter.
+ *
+ * @param filter filter which should be extended
+ * @param key key that should be constrained
+ * @param constraintOperator determines how the key should be filtered.
+ * @param constraintValue value which corresponds to the filter.
+ * @param booleanOperator determines how to link keyconstraint to the existing filter (either as a conjunction or disjunction).
+ */
 void filter_AddConstraintToFilter(Filter *filter, Key key, ConstraintOperator constraintOperator, int constraintValue, BooleanOperator booleanOperator);
 
 #endif // FILTERMODEL_H
